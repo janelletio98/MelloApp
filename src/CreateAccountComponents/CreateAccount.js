7ad47d4cac93550209518d5ef4e8a2d50c8806a9
Jorge Verduzco <62412721+Jrge-V@users.noreply.github.com>
2022-11-17 15:18:03 -0800

    Sprint2Mello

---
+++ src/CreateAccountComponents/CreateAccount.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import "../Styles/CreateAccount.css"
+
+
+/*function CreateAccount (){
+
+    let navigate = useNavigate();
+    return (
+        <div>  
+            <input type="text" placeholder='Username' />
+            <input type="text" placeholder='Password' />
+            <button onClick={() => 
+                {navigate('./Components');
+                }}
+                > 
+                Create Account 
+                </button>
+        </div>
+    );
+}
+
+export {CreateAccount}; */
