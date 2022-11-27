9f02096fd86682d4a6a4e11b39f157804886c395
InLikeFlynn <outmost@me.com>
2022-11-26 19:22:05 -0800

    backend start && frontend start

---
+++ backend/data/dummy.js
@@ -0,0 +1,36 @@
+const dummy = [
+	{
+		id: '1',
+		songTitle: 'Song Name',
+		songArtist: 'Artist Name',
+		songGenre: 'Genre Type',
+		content: 'song image',
+		category: 'College',
+	},
+	{
+		id: '2',
+		songTitle: 'Song Name',
+		songArtist: 'Artist Name',
+		songGenre: 'Genre Type',
+		content: 'song image',
+		category: 'Learning',
+	},
+	{
+		id: '3',
+		songTitle: 'Song Name',
+		songArtist: 'Artist Name',
+		songGenre: 'Genre Type',
+		content: 'song image',
+		category: 'Entertainment',
+	},
+	{
+		id: 4,
+		songTitle: 'Song Name',
+		songArtist: 'Artist Name',
+		songGenre: 'Genre Type',
+		content: 'song image',
+		category: 'Learning',
+	},
+];
+
+module.exports = dummy;
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dummy = require('./data/dummy');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+//end point
+app.get('/', (req, res) => {
+	res.send('API is now running');
+});
+
+//requesting dummy data from back end
+app.get('/api/dummy', (req, res) => {
+	res.json(dummy);
+});
+
+app.get('/api/dummy/:id', (req, res) => {
+	const note = dummy.find((n) => n.id === req.params.id);
+	//console.log(req.params);
+	res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
