e66453dbafd4cacd6e41617ee7db209e57f3394c
JorgeV <jorgeverduzco91@gmail.com>
2022-11-28 13:29:01 -0800

    test test

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import './App.css';
 import { LeftMenu } from './Components/LeftMenu';
 import { MainContainer } from './Components/MainContainer';
 import { RightMenu } from './Components/RightMenu';
+import { FrontPage } from './Components/FrontPage';
+import { Route, Link } from 'react-router-dom';
 
 
+
+
+
+/*NOTE FOR ROUTES: ANYTHING OUTSIDE THE ROUTE TAGS WILL BE DISPLAYED IN ALL PAGES. 
+ANYTHING INSIDE THE ROUTE TAGS WILL BE DISPLAYED FOR A SPECIFIC PAGE */
+//INSIDE ALL ROUTES TAG, WE WILL HAVE AN INDIVIDUAL ROUTE
 
 function App() {
   return (
+
+
     <div className="App">
 
-
+      
       
 
       <LeftMenu/>
       <MainContainer/>
       <RightMenu/>
+      
+      
+      
+      
+    
 
       <div className="background"></div>
+
+
+
     </div>
   );
 }
---
+++ src/Components/FrontPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../Styles/FrontPage.css"
+
+function FrontPage() {
+  return <div className = 'frontPage'>
+
+  <li>
+    <a href='/'>Loginfjekgeuirgi</a>
+    
+  </li>
+
+
+
+</div>
+}
+
+export  {FrontPage}
