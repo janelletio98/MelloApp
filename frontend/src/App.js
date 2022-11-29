09f083f7763ab96e7a771d2f8d23e9dd7c631622
InLikeFlynn <saidshaldan@gmail.com>
2022-11-29 15:32:55 -0800

    bootstrap header/footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 
-const App = () => <h1 className="App"> Hello World </h1>;
+const App = () => (
+	<>
+		<Header />
+		<main style={{ minHeight: '93vh' }}></main>
+		<Footer />
+	</>
+);
 
 export default App;
