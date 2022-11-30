c943d777169852a7e39b6dd8836c9d870626454f
InLikeFlynn <saidshaldan@gmail.com>
2022-11-29 23:27:02 -0800

    header green

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-	return <div style={{ color: 'pink' }}>Header</div>;
+	return <div style={{ color: 'green' }}>Header</div>;
 };
 
 export default Header;
